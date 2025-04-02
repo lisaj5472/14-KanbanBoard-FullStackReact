56ca91482f2288e9681ae5f97e7e14eb0ed357bc
LisaJ5472 <lisaj5472@gmail.com>
2025-04-02 17:39:16 -0600

    updated client API to production site

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -3,13 +3,16 @@ import { UserLogin } from "../interfaces/UserLogin";
 // TODO: make a POST request to the login route
 const login = async (userInfo: UserLogin) => {
   try {
-    const response = await fetch("http://localhost:3001/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userInfo),
-    });
+    const response = await fetch(
+      "https://one4-kanbanboard-fullstackreact.onrender.com/auth/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userInfo),
+      }
+    );
 
     const data = await response.json();
 
